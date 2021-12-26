6fbf955c79e27b5c113ac5c73ee12d35c348a6da
hyia <grsky360@live.com>
2021-12-26 22:21:28 +0800

    Init

---
+++ src/native-test/java/ilio/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package ilio;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
