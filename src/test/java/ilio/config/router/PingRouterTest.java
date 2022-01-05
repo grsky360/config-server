8e073e252f71e54f02df40e10c2a5d36389def17
qingyang.zhong <qizhong@coupang.com>
2022-01-05 18:03:03 +0800

    Switch to maven

--- src/test/java/ilio/config/router/PingRouterTest.java
+++ src/test/java/ilio/config/router/PingRouterTest.java
@@ -1,4 +1,4 @@
-package ilio;
+package ilio.config.router;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -7,15 +7,15 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+public class PingRouterTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testPing() {
         given()
-            .when().get("/hello")
+            .when().get("/api/ping")
             .then()
             .statusCode(200)
-            .body(is("Hello RESTEasy"));
+            .body("data", is("pong"));
     }
 
 }
